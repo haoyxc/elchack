ff23a79dbfdb705fb4120199f0e40bd58c591e14
Izzy Kim <kimizzy@seas.upenn.edu>
2019-10-12 14:53:45 -0400

    back to before

--- screens/Home.js
+++ screens/Home.js
@@ -11,7 +11,7 @@ import {
   FlatList,
   ImageBackground
 } from "react-native";
-import img from "./img/StartingScreen.jpg";
+import img from "./img/lipstickbg.jpg";
 
 function Home({ navigation }) {
   function handlePress() {
